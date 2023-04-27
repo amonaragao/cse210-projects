9a3d0917f6078a59cce17cbb8014c40a6c93f058
amonaragao <amonbastos@gmail.com>
2023-04-27 15:10:12 -0700

    Lists C#

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
-using System;
+List<int> numbers = new List<int>();
 
-class Program
+int newNumber = -1;
+while (newNumber != 0)
 {
-    static void Main(string[] args)
+    Console.Write("Enter a new number (if you finished enter 0): ");
+    newNumber = int.Parse(Console.ReadLine());
+
+    if (newNumber != 0)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        numbers.Add(newNumber);
     }
 }
+
+int sum = 0;
+foreach (int number in numbers)
+{
+    sum += number;
+}
+
+Console.WriteLine($"The sum is: {sum}");
+
+float average = ((float)sum) / numbers.Count;
+Console.WriteLine ($"The average is: {average}");
+
+int largest = numbers[0];
+
+foreach (int number in numbers)
+{
+    if (number > largest)
+    {
+        largest = number;
+    }
+}
+
+Console.WriteLine($"The largest is: {largest}");
