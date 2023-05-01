f4d2737841ea86e295cd5a2c19c22024ec065931
amonaragao <amonbastos@gmail.com>
2023-05-01 15:10:47 -0700

    Classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,26 @@
 using System;
-
+using Learning02;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2021;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = " Microsoft";
+        job2._startYear = 2019;
+        job2._endYear =  2020;
+
+        Resume resume1 = new Resume();
+        resume1._name = "Charles Smith";
+        resume1._jobsList.Add(job1);
+        resume1._jobsList.Add(job2);
+
+        resume1.Display();
     }
 }
